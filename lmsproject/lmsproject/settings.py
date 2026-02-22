51cd410408dfe77a8171e2beff25191b417a07b3
Mazhar <mazharhusain9919@gmail.com>
2026-02-23 00:16:18 +0530

    add

--- lmsproject/lmsproject/settings.py
+++ lmsproject/lmsproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,8 +114,10 @@ import os
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'lmsapp/static'),
 ]
 
 
--- lmsproject/lmsproject/urls.py
+++ lmsproject/lmsproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,7 @@ urlpatterns = [
     path('studentapp/',include(('studentapp.studentappurls','studentapp'),namespace='studentapp')),
     path('adminapp/',include(('adminapp.adminappurls','adminapp'),namespace='adminapp')), 
 ]
+
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
